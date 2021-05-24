e2156ee1a8c27b1f6e9a2ebb6e5f295a3dca1c87
vishvesh-jain <49018485+vishvesh-jain@users.noreply.github.com>
2021-05-24 23:30:06 +0530

    update

---
+++ assets/js/Product-Slider-3-1.js
@@ -0,0 +1,20 @@
+	$(document).ready(function() {
+
+			
+			$('#slider').layerSlider({
+				sliderVersion: '6.0.6',
+				type: 'fullwidth',
+				autoStart: false,
+				keybNav: false,
+				touchNav: false,
+				skin: 'v6',
+				navPrevNext: false,
+				hoverPrevNext: false,
+				navStartStop: false,
+				navButtons: false,
+				showCircleTimer: false,
+				thumbnailNavigation: 'disabled',
+				allowRestartOnResize: true,
+				skinsPath: '../../layerslider/skins/'
+			});
+		});
